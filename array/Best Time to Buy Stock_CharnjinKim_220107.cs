206b546fa6df9aa29d30ae0703d44fca4ff38bc9
Celenort <celenort@snu.ac.kr>
2022-01-07 21:48:55 +0900

    Added array problems_1

---
+++ array/Best Time to Buy Stock_CharnjinKim_220107.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace BestTimeToBuyStock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution su = new Solution();
+            int[] prices = new int[] { 5, 6, 7, 3, 4, 5 };
+            int a = su.MaxProfit(prices);
+            Console.WriteLine(a);
+        }
+    }
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int res = 0;
+            int min = 99999;
+            for (int i = 0;i<prices.Length;i++)
+            {
+                if (prices[i]<min)
+                {
+                    min = prices[i];
+                }
+                else if (res < prices[i] - min)
+                {
+                    res = prices[i] - min;
+                }
+            }
+            return res;
+        }
+
+    }
+}
