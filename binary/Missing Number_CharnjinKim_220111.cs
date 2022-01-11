1d431870810304983b390b64b9d2c390c62d4a92
Celenort <celenort@snu.ac.kr>
2022-01-11 11:53:03 +0900

    array & binary problems_220111 김찬진

---
+++ binary/Missing Number_CharnjinKim_220111.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MissingNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class Solution {
+        public int MissingNumber(int[] nums) {
+            int a = nums.Length *(nums.Length+1) / 2;
+            int tot=0;
+            for (int i = 0;i<nums.Length;i++) {
+                tot+=nums[i];
+            }
+            return a-tot;
+        }
+    }
+}
---
+++ binary/Number of 1 Bits_CharnjinKim_220111.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Numberof1Bits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class Solution {
+        public int HammingWeight(uint n) {
+            int i = 0;
+            for (int p = 0;p<32;p++) {
+                if(Convert.ToBoolean(n%2)) {
+                i++;
+            } 
+            n/=2;    
+            }
+            return i;
+
+        }
+    }
+}
