80bda4ec828302f43fe39517310fc9e8e75b75a4
Celenort <codefine.asd@gmail.com>
2022-02-26 22:27:02 +0900

    graph, interval little

---
+++ graph/CloneGraph/CloneGraph_CharnjinKim_220226.cs
@@ -0,0 +1,16 @@
+﻿public class Solution 
+{
+    Dictionary<int, Node> nodemap = new Dictionary<int, Node>();
+    public Node CloneGraph(Node node) {
+        if (node==null) {
+            return null;
+        }
+        if (!nodemap.ContainsKey(node.val)) {
+            nodemap[node.val]= new Node(node.val, new List<Node>());
+            foreach (var neighbor in node.neighbors) {
+                nodemap[node.val].neighbors.Add(CloneGraph(neighbor)); 
+            }
+        }
+        return nodemap[node.val];
+    }
+}
