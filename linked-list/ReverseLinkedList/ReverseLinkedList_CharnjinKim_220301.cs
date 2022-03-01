ff31643e7ca5fffc05c7406ce81a23b671640a5c
Celenort <celenort@snu.ac.kr>
2022-03-02 01:34:15 +0900

    Solved LinkedList(CJKim)

---
+++ linked-list/ReverseLinkedList/ReverseLinkedList_CharnjinKim_220301.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        if (head==null) {
+            return head;
+        }
+        List<ListNode> res = new List<ListNode>();
+        ListNode prevNext = null;
+        while (head.next!=null) {
+            res.Add(new ListNode(head.val, prevNext));
+            prevNext = res[res.Count-1];
+            head = head.next;
+        }
+        res.Add(new ListNode(head.val, prevNext));
+        return res[res.Count-1];
+    }
+}
