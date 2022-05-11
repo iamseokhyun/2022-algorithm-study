e0244549a4d587558d83f6e99527fa86f419657d
Wooyeol Lee <97590283+thisisWooyeol@users.noreply.github.com>
2022-05-11 13:41:43 +0900

    add additional solution using "inorder traversal"

--- tree/ValidateBinarySearchTree/ValidateBinarySearchTree_WooyeolLee_220508.cpp
+++ tree/ValidateBinarySearchTree/ValidateBinarySearchTree_WooyeolLee_220508.cpp
@@ -1,3 +1,4 @@
+// algorithm ver.1
 bool isValidBST(TreeNode* root, long lower = LONG_MIN, long upper = LONG_MAX) {
     bool isValid = true;
         
@@ -20,4 +21,24 @@ bool isValidBST(TreeNode* root, long lower = LONG_MIN, long upper = LONG_MAX) {
   : The left subtree of a node contains only nodes with keys **less than** the node's key.
   : The right subtree of a node contains only nodes with keys **greater than** the node's key.
 - LONG_MIN and LONG_MAX provide default bound of node’s value.
+*/
+
+
+
+// algorithm ver.2
+bool isValidBST(TreeNode* root) {    
+    TreeNode* prev = nullptr;      
+    return validate(root, prev);    
+}    
+bool validate(TreeNode* root, TreeNode* &prev){    
+    if(root == nullptr) return true;    
+    if(!validate(root->left, prev)) return false;    
+    if(prev != nullptr && prev->val >= root->val) return false;    
+    prev = root;    
+    return validate(root->right, prev);    
+} // runtime : 14 ms (faster than 67.92 %), memory usage : 21.7 MB (less than 33.23 %)
+
+/*
+- Solution using “inorder traversal” of binary tree. -> O(N) (Idea from leetcode discussion)
+  → neat code compared to algorithm above.
 */
